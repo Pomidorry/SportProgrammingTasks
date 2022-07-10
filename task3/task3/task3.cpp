a1c6c49ea9d778cc84219d0c566b4d0c2c076ba8
Pomidorry <106489913+Pomidorry@users.noreply.github.com>
2022-07-10 14:33:28 +0300

    task 3 and 4

---
+++ task3/task3/task3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+void Pair(int arr[], int n, int t) {
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] + arr[j] == t)
+            {
+                cout << arr[i] << " " << arr[j] << endl;
+                return;
+            }
+        }
+    }
+    cout << "Pair not found" << endl;
+}
+int main()
+{
+    int arr[100];
+    int n;
+    cin >> n;
+    int t;
+    cin >> t;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    Pair(arr, n, t);
+}
+
---
+++ task4/task4/task4.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <iostream>
+using namespace std;
+struct binaryTree {
+    int key;
+    binaryTree* left, * right;
+    binaryTree() {
+        cin >> key;
+        this->key = key;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+bool Identical(binaryTree* a, binaryTree* b)
+{
+    if (a == nullptr && b == nullptr) {
+        return true;
+    }
+
+    return (a && b) && (a->key == b->key) &&
+        Identical(a->left, a->left) &&
+        Identical(b->right, b->right);
+}
+int main()
+{
+    binaryTree* x = nullptr;
+    x = new binaryTree();
+    x->left = new binaryTree();
+    x->right = new binaryTree();
+    x->left->left = new binaryTree();
+    x->left->right = new binaryTree();
+    x->right->left = new binaryTree();
+    x->right->right = new binaryTree();
+    binaryTree* y = nullptr;
+    y = new binaryTree();
+    y->left = new binaryTree();
+    y->right = new binaryTree();
+    y->left->left = new binaryTree();
+    y->left->right = new binaryTree();
+    y->right->left = new binaryTree();
+    y->right->right = new binaryTree();
+    if (Identical(x, y)) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
+}
+

64cafebcfa85f7c31b089bfd00a114f3955e8607
Pomidorry <106489913+Pomidorry@users.noreply.github.com>
2022-07-10 13:39:32 +0300

    task1

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+void sort(int arr[], int n){
+	int z = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] == 0) {
+			z++;
+		}
+	}
+	for (int i = 0; i < z; i++) {
+		arr[i] = 0;
+	}
+	for (int i = z; i < n; i++) {
+		arr[i] = 1;
+	}
+}
+int main() {
+	int A[100];
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> A[i];
+	}
+	sort(A, n);
+	for (int i = 0; i < n; i++) {
+		cout<< A[i]<<" ";
+	}
+}

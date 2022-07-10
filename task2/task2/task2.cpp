6c9f5e8a4b0651365e9377ffd979b4344d7a94c1
Pomidorry <106489913+Pomidorry@users.noreply.github.com>
2022-07-10 14:00:02 +0300

    task2

---
+++ task2/task2/task2.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+void printSubs(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += a[j];
+            if (sum == 0) {
+                cout << "{" << a[i] << "..." << a[j] << "}"<<endl;
+            }
+        }
+    }
+}
+int main()
+{
+    int A[100];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    printSubs(A, n);
+}
+
